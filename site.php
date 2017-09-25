4c6d9edc27f8a6e10014a3da8027bae0a9d3ac44
Omar Calderon <omar.calderon>
2017-09-25 16:34:23 -0700

    Agregado site.php para conectar a la base d datos

---
+++ site.php
@@ -0,0 +1,15 @@
+<?php
+
+$config = [
+  'domain'  => 'www.almacen.com',
+  'db_auth' => '',
+  'db_host' => 'localhost',
+  'db_name' => 'almacen',
+  'db_user' => 'root',
+  'db_pass' => 'root',
+  'env'     => 'debug',
+  'locale'  => 'es_MX',
+  'site_name' => 'Almacen',
+  'timezone' => 'America/Mexico_City'
+];
+?>
