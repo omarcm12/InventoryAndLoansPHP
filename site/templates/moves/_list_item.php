04833ca6b6d4553a9c0d0b8a384696a48632abd5
Uriel Cebreros <urielcebreros@gmail.com>
2017-11-21 10:00:39 -0800

    Colores de alta y baja en movimientos

--- site/templates/moves/_list_item.php
+++ site/templates/moves/_list_item.php
@@ -1,12 +1,15 @@
 <tr>
   <td><?= $move->NoOrder() ?></td>   
   <td>
-    <a href="/admin/inventario/<?= $move->Material()->ID()?>">
-      <?= $move->Material()->Name() ?>
+    <a href="/admin/inventario/<?= $move->Material()->ID()?>" style="color:#607d8b">
+      <?= $move->Material()->CatalogNumber() ?> - <?= $move->Material()->Name() ?>
     </a>
   </td>   
   <td><?= $move->Pieces() ?></td>
-  <td><?= $move->TypeName() ?></td>
+  <td style="color: <?= $move->Type() == MOVE_TYPE_ADD ? '#109853' : '#d9534f' ?>">
+      <?= $move->TypeName() ?> 
+      <i class="fa fa-arrow-<?= $move->Type() == MOVE_TYPE_ADD ? 'up' : 'down' ?>" aria-hidden="true"></i>
+  </td>
   <td><?= $move->User()->FullName() ?></td>  
   <td><?= $move->Description() ?></td>
   <td><?= $move->CreatedAtFormatted() ?></td>
