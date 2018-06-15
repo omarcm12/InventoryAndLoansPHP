c7c7a3294c240d0d69d15bda953c9058aa7e978d
Omar Calderon <omar.calderon@uabc.edu.mx>
2018-06-14 20:15:11 -0800

    reporte bitacora y moves

--- site/templates/binnacle/modal.php
+++ site/templates/binnacle/modal.php
@@ -1,12 +1,14 @@
- <div class="modal fade" id="myModal-move-<?= $move->ID()?>">
-<div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-          <h3 class="modal-title">Materiales <?= $move->TypeName() ?> - <?= $move->ID()?></h3>
-        </div>
-        <div class="modal-body">
+<div class="modal fade" id="modal-deliver-<?= $move->ID()?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 id="move-modal-title" class="modal-title">Regresar Materiales</h4>        
+      </div>
+      <div id="move-response-message" class="api-message label-danger" style="display:none;">Error</div>
       
+      <div class="modal-body" style="overflow: scroll;height: 400px;">      
           <table class="table table-striped" id="tblGrid">
             <thead id="tblHead">
               <tr>
@@ -31,12 +33,13 @@
             <?php endforeach ?>
             </tbody>
           </table>
+
+      </div>
+      <div class="modal-footer">
+       
+        <button type="button" class="btn btn-info btn-sm" data-dismiss="modal">Cancelar</button>
+      </div>
     </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default " data-dismiss="modal">Close</button>
-        </div>
-        
-      </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-  </div><!-- /.modal -->
-  </td>
+
+  </div>
+</div>
--- site/templates/moves/_list_item.php
+++ site/templates/moves/_list_item.php
@@ -12,7 +12,11 @@
   </td>
   <td><?= $move->User()->FullName() ?></td>  
   <td><?= $move->Age() ?></td>
-  <td><button class="btn btn-info btn-sm" type="button"  data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-search" aria-hidden="true"></button></td>
+  <td>
+   <!-- <button class="btn btn-info btn-sm" type="button"  data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-search" aria-hidden="true"></button>-->
+    <a class="btn btn-info btn-sm btn-fill btn-uabc-green" data-toggle="modal" data-target="#modal-<?= $move->ID()?>"><span class="glyphicon glyphicon-search" aria-hidden="true"></a>
+
+  </td>
   
 
 
